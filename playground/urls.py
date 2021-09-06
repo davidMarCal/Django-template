f8106bcec23c3cdf9298d4b631de945b6ec18b9d
davidmartinUsal <david_matin@usal.es>
2021-09-06 12:15:37 +0200

    First commit

--- playground/urls.py
+++ playground/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns=[
     path('hello/',views.say_hello),
     path('index/',views.show_index),
-    path('news-detail/',views.show_news_detail)
+    path('news-detail/',views.show_news_detail),
+    path('user/login/',views.show_login),
+    path('user/register/',views.show_register)
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 #----------------
 #REQUEST HANLDER
 #---------------
 
 # Create your views here.
-
-
 def say_hello(request):
     x=1
     y=2
@@ -19,3 +19,9 @@ def show_index(request):
 
 def show_news_detail(request):
     return render(request,'news-detail.html')
+
+def show_login(request):
+    return render(request,'user/login.html') 
+
+def show_register(request):
+    return render(request,'user/register.html') 
