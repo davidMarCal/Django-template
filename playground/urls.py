be44201f1fb6d6104c7bf6df5480277546121c79
davidmartinUsal <david_matin@usal.es>
2021-09-07 12:36:33 +0200

    Login system implemented

--- playground/urls.py
+++ playground/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 #URLConf
 urlpatterns=[
+    #GET
     path('hello/',views.say_hello),
     path('index/',views.show_index),
     path('news-detail/',views.show_news_detail),
     path('user/login/',views.show_login),
-    path('user/register/',views.show_register)
+    path('user/register/',views.show_register),
+    path('user/logOut/',views.logOut),
+    
+    #POST
+    path('user/signUp',views.signUp),
+    path('user/signIn',views.signIn),
+    
 ]
